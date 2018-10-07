64f29bd1e8e3da412d39255de449e4a026fb3ced
Piotr Kedziora <piotrkedziorapl@gmail.com>
2018-10-07 13:48:08 +0200

    Add structure with template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <header>
+            <h1>Contact list</h1>
+            <div>
+                Add contact: <button>+</button>
+            </div>
+            <div>
+                Search contact: <input type="text"/>
+            </div>
         </header>
+        <main>
+            <div>
+                <p>
+                    <span>Piotr</span><br/>
+                    <span>4485691564</span>
+                </p>
+                <button>Edytuj</button><button>Usuń</button>
+            </div>
+            <div>
+                <p>
+                    <span>Piotr</span><br/>
+                    <span>4485691564</span>
+                </p>
+                <button>Edytuj</button><button>Usuń</button>
+            </div>
+            <div>
+                <p>
+                    <span>Piotr</span><br/>
+                    <span>4485691564</span>
+                </p>
+                <button>Edytuj</button><button>Usuń</button>
+            </div>
+        </main>
+        <dialog open>
+            name: <input type="text"/><br/>
+            phone: <input type="tel" /><br/>
+            <button>Ok</button> <button>Anuluj</button>
+        </dialog>
       </div>
     );
   }
