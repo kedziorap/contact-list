b5f4b3d127ede9ab16ee2987f26e39a1d214bd2a
Piotr Kedziora <piotrkedziorapl@gmail.com>
2018-10-08 10:31:31 +0200

    Fix editing

--- src/App.js
+++ src/App.js
@@ -50,10 +50,13 @@ class App extends Component {
   }
   addNewContact = (name, phone) =>{
     const text = this.state.text;
-    console.log(name, phone)
     const checkPhone = this.checkPhone(phone);
     const checkName = this.checkName(name);
-    if (checkName === 1 && checkPhone === 1) {
+    let errors = [];
+    if (checkPhone !==1) errors = [...errors, checkPhone];
+    if (checkName !==1) errors = [...errors, checkName];
+    if(errors.length) return errors;
+    if (checkPhone === 1 && checkPhone === 1) {
       if(name.includes(text) || phone.includes(text)) {
         this.setState({
           users: [...this.state.users, {name, phone}],
@@ -66,7 +69,6 @@ class App extends Component {
       }
       return 1
     }
-    return [checkPhone, checkName];
   }
   deleteContact = phone => {
     const users = this.state.users;
@@ -76,8 +78,21 @@ class App extends Component {
       filteredUsers: updateUser
     })
   }
-  editContact = (name, phone, oldPhone) => {
-    const allUsers = this.state.users;
+  editContact = (name, phone, oldPhone, oldName) => {
+    let errors = []
+    //console.log(this.checkName(name));
+    if (oldName !== name) {
+      const nameRes = this.checkName(name);
+      if (nameRes !== 1) errors = [...errors, nameRes]
+    } 
+    if (oldPhone !== phone) {
+      const phoneRes = this.checkPhone(phone);
+      if (phoneRes !== 1) errors = [...errors, phoneRes]
+    } 
+    if (errors.length) {
+      return errors
+    } else {
+      const allUsers = this.state.users;
       const users = allUsers.map(user =>{
         if(user.phone === oldPhone) {
           user.name = name;
@@ -89,6 +104,8 @@ class App extends Component {
         users,
         filteredUsers: users
       });
+      return 1
+    }
   }
   editionInProgress = () => {
     this.setState({
@@ -97,6 +114,8 @@ class App extends Component {
   }
   checkName = name => {
     name = name.trim();
+    const filter = (this.state.users).filter(user => user.name === name);
+    if (filter.length) return 'This name is actualy in use!';
     const reg = new RegExp('[A-z0-9 -]');
     if (reg.test(name) && name.length > 2) {
       return 1
@@ -107,8 +126,10 @@ class App extends Component {
   }
 
   checkPhone = phone => {
-    const reg = new RegExp('[0-9]');
+    const reg = new RegExp('^[0-9]+$');
     phone=phone.trim();
+    const filter = (this.state.users).filter(user => user.phone === phone);
+    if (filter.length) return 'Contact with this number already exists!!';
     if (reg.test(phone) && phone.length > 5) {
       return 1
     } else if (phone.length <=5 ) {
--- src/components/ContactsList/Contact/Contact.js
+++ src/components/ContactsList/Contact/Contact.js
@@ -6,16 +6,19 @@ class Contact extends Component {
         this.state = {
             isEditing: false,
             name: props.name,
-            phone: props.phone
+            phone: props.phone,
+            errors: []
         };
     }
     render(){
+        const errors = this.state.errors.length ? 1 : null;
         if(this.state.isEditing) {
             return(
                 <div>
                     <p>
                         <input type="text" value={this.state.name} onChange={this.onChangeName} placeholder="Name"/>   <br/>
-                        <input type="text" value={this.state.phone} onChange={this.onChangePhone} placeholder="Phone number"/>
+                        <input type="text" value={this.state.phone} onChange={this.onChangePhone} placeholder="Phone number"/> <br/>
+                        {errors && (this.state.errors).map(err => <span className="error" key={err}>{err}<br/></span>)}
                     </p>
                     <button onClick={this.editInputs}>Ok</button><button onClick={this.cancelEditing}>Cancel</button>
                 </div>
@@ -41,12 +44,21 @@ class Contact extends Component {
     editInputs = () => {
         const {name, phone} = this.state;
         const oldPhone = this.props.phone;
-        this.props.onEdit(name, phone, oldPhone);
-        this.toggleEdit();
+        const oldName = this.props.name;
+        const result = this.props.onEdit(name, phone, oldPhone, oldName);
+        console.log(result);
+        if (result === 1) {
+            this.toggleEdit();
+        } else {
+            this.setState({
+                errors: [...result]
+            })
+        }
     }
     toggleEdit = () => {
         this.setState({
-            isEditing: !this.state.isEditing
+            isEditing: !this.state.isEditing,
+            errors: []
         }, this.props.editionInProgress());
     }
     onChangeName = e =>{
