953871e256406e1511661cc5d69490c7057293af
Piotr Kedziora <piotrkedziorapl@gmail.com>
2018-10-09 14:32:32 +0200

    Updates

---
+++ precache-manifest.ce56e1f73defee521de43220e2f59eca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "377b31ea3711a9d6ba50",
+    "url": "/contact-list/static/js/runtime~main.377b31ea.js"
+  },
+  {
+    "revision": "0de6593d23887e2d633a",
+    "url": "/contact-list/static/js/main.0de6593d.chunk.js"
+  },
+  {
+    "revision": "08afc1cfbb1629847c98",
+    "url": "/contact-list/static/js/1.08afc1cf.chunk.js"
+  },
+  {
+    "revision": "0de6593d23887e2d633a",
+    "url": "/contact-list/static/css/main.02155a53.chunk.css"
+  },
+  {
+    "revision": "f064137be00296b8300fc661c1916161",
+    "url": "/contact-list/index.html"
+  }
+];
